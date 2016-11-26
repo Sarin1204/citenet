8a8cfd23e7237a526178ef46ddf2ac55026045d7
Vipul Sarin <vipul.sarin@gmail.com>
2016-11-26 13:52:53 -0700

    Move alchemy config to json,fix typeahead bug

--- web_module/public/shortest_path/controllers/shortest_path.client.controller.js
+++ web_module/public/shortest_path/controllers/shortest_path.client.controller.js
@@ -2,8 +2,8 @@
  * Created by vipul on 4/27/2016.
  */
 angular.module('shortest_path').controller('ShortestPathController',['$scope',
-    '$routeParams', '$location','ShortestPath','getUser','alertify',
-    function($scope, $routeParams, $location, ShortestPath,getUser,alertify){
+    '$routeParams', '$location','ShortestPath','getUser','alertify','$http',
+    function($scope, $routeParams, $location, ShortestPath,getUser,alertify,$http){
         console.log("getUser"+JSON.stringify(getUser.user));
         $scope.all_paths = false;
         $scope.intermediate_nodes = true;
@@ -21,77 +21,17 @@ angular.module('shortest_path').controller('ShortestPathController',['$scope',
             ShortestPath.getShortestPath.query(fields, function(response){
                 console.log('Success in getShortestPath '+JSON.stringify(response));
                 $scope.shortest_path_json = response[0]["graph"];
-                var myEl = angular.element("svg");
-                myEl.remove();
-                var config = {
-                    dataSource : response[0]["graph"],
-                    nodeCaption: 'caption',
-                    forceLocked:false,
-                    alpha: 0.2,
-                    edgeCaption: 'caption',
-                    nodeCaptionsOnByDefault: true,
-                    edgeCaptionsOnByDefault: true,
-                    nodeTypes: {"type":["Paper","subject_area","author","affiliation"]},
-                    edgeTypes: {"caption":["CITES","associated_to","affiliated_to","written_by"]},
-                    directedEdges:true,
-                    linkDistancefn: function(){ return 800; },
-                    nodeStyle: {
-                        "Paper": {
-                            "color":'#1B9E77',
-                            "borderColor": "#136d52",
-                            "borderWidth": 6,
-                            "radius"     : 44
-                        },
-                        "subject_area": {
-                            "color":'#D95F02',
-                            "borderWidth": 6,
-                            "borderColor": "#ad4b00",
-                            "radius"     : 44
-                        },
-                        "author": {
-                            "color":'#eded5c',
-                            "borderWidth": 6,
-                            "borderColor": "#a3a33f",
-                            "radius"     : 44
-                        },
-                        "affiliation": {
-                            "color":'#c060c1',
-                            "borderWidth": 6,
-                            "borderColor": "#904891",
-                            "radius"     : 44
-                        }
-                    },
-                    edgeStyle: {
-                        "all":{
-                          "opacity":0.4
-                        },
-                        "CITES": {
-                            "color": "#990921",
-                            "width":4,
-                            "borderWidth": 10,
-                        },
-                        "associated_to": {
-                            "color": "#2e377c",
-                            "width":4
-                        },
-                        "affiliated_to": {
-                            "color": "#2a6815",
-                            "width":4
-                        },
-                        "written_by":{
-                            "color": "rgb(0,255,0)",
-                            "width":4
-                        }
-                    },
-                    initialScale: 0.5,
-                    initialTranslate: [250,150],
-                    zoomControls:true,
-                    edgeOverlayWidth: 30
-                };
-                //alchemy.begin({"dataSource": response[0]["graph"]})
-                alchemy = new Alchemy(config);
-                alertify.logPosition("top right");
-                alertify.success("Success! Check out your graph!");
+                $http.get('../alchemy_config.json')
+                    .then(function(res){
+                        var config = res.data;
+                        config.dataSource = response[0]["graph"];
+                        var myEl = angular.element("svg");
+                        myEl.remove();
+                        //alchemy.begin({"dataSource": response[0]["graph"]})
+                        alchemy = new Alchemy(config);
+                        alertify.logPosition("top right");
+                        alertify.success("Success! Check out your graph!");
+                    })
             },function(error){
                 console.log('Error in getShortestPath '+error);
                 alertify.logPosition("top right");
--- web_module/public/sphere_influence/controllers/sphere_influence.client.controller.js
+++ web_module/public/sphere_influence/controllers/sphere_influence.client.controller.js
@@ -2,8 +2,8 @@
  * Created by Dhruvraj on 4/27/2016.
  */
 angular.module('sphere_influence').controller('SphereInfluence',['$scope',
-    '$routeParams', '$location','SphereInfluence','getUser',
-    function($scope, $routeParams, $location, SphereInfluence,getUser){
+    '$routeParams', '$location','SphereInfluence','getUser','$http',
+    function($scope, $routeParams, $location, SphereInfluence,getUser,$http){
         console.log("getUser"+JSON.stringify(getUser.user));
         $scope.all_paths = false;
         $scope.intermediate_nodes = true;
@@ -21,70 +21,15 @@ angular.module('sphere_influence').controller('SphereInfluence',['$scope',
             SphereInfluence.getSphereInfluence.query(fields, function(response){
                 console.log('Success in getSphereInfluence '+JSON.stringify(response));
                 $scope.sphere_influence_json = response[0]["graph"];
-                var myEl = angular.element("svg");
-                myEl.remove();
-                var config = {
-                    dataSource : response[0]["graph"],
-                    nodeCaption: 'caption',
-                    forceLocked:false,
-                    edgeCaption: 'caption',
-                    nodeCaptionsOnByDefault: true,
-                    edgeCaptionsOnByDefault: true,
-                    nodeTypes: {"type":["Paper","subject_area","author","affiliation"]},
-                    edgeTypes: {"caption":["CITES","associated_to","affiliated_to","written_by"]},
-                    directedEdges:true,
-                    linkDistancefn: function(){ return 800; },
-                    nodeStyle: {
-                        "Paper": {
-                            "color":'#1B9E77',
-                            "borderColor": "#136d52",
-                            "borderWidth": 6,
-                            "radius"     : 44
-                        },
-                        "subject_area": {
-                            "color":'#D95F02',
-                            "borderWidth": 6,
-                            "borderColor": "#ad4b00",
-                            "radius"     : 44
-                        },
-                        "author": {
-                            "color":'#eded5c',
-                            "borderWidth": 6,
-                            "borderColor": "#a3a33f",
-                            "radius"     : 44
-                        },
-                        "affiliation": {
-                            "color":'#c060c1',
-                            "borderWidth": 6,
-                            "borderColor": "#904891",
-                            "radius"     : 44
-                        }
-                    },
-                    edgeStyle: {
-                        "CITES": {
-                            "color": "#990921",
-                            "width":2,
-                            "borderWidth": 10
-                        },
-                        "associated_to": {
-                            "color": "#2e377c",
-                            "width":1
-                        },
-                        "affiliated_to": {
-                            "color": "#2a6815",
-                            "width":1
-                        },
-                        "written_by":{
-                            "color": "rgb(0,255,0)",
-                            "width":1
-                        }
-                    },
-                    initialScale: 1.4,
-                    initialTranslate: [250,150],
-                    zoomControls:true
-                };
-                //alchemy.begin({"dataSource": response[0]["graph"]})
-                alchemy = new Alchemy(config);
+                $http.get('../alchemy_config.json')
+                    .then(function(res){
+                        var config = res.data;
+                        config.dataSource = response[0]["graph"];
+                        var myEl = angular.element("svg");
+                        myEl.remove();
+                        //alchemy.begin({"dataSource": response[0]["graph"]})
+                        alchemy = new Alchemy(config);
+                    })
             },function(error){
                 console.log('Error in getSphereInfluence '+JSON.stringify(error));
             });
