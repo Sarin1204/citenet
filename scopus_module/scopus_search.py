c689eb1b978ed312b3f677f5dfba40f5c0cc8ac1
Vipul Sarin <vipul.sarin@gmail.com>
2016-12-02 14:42:17 -0700

    Add upload paper module changes back

--- scopus_module/scopus_search.py
+++ scopus_module/scopus_search.py
@@ -1,7 +1,8 @@
 import pdb
 from scopus.scopus_api import ScopusAbstract
 
-ab = ScopusAbstract("2-s2.0-84930662492")
+ab = ScopusAbstract("2-s2.0-84938117894")
+pdb.set_trace()
 for au in ab.authors:
     print(au)
     print([x for x in dir(au) if '__' not in x])
